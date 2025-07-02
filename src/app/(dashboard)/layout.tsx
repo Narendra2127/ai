8b6fc409d21248285e4c1e925d4cfc42c491e413
Narendra2127 <narendra.nw21@gmail.com>
2025-07-03 00:16:43 +0530

    dashboard navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import { Sidebar, SidebarProvider } from "@/components/ui/sidebar"
+import { DashboardNavbar } from "@/module/dashboard/ui/components/dashboard-navbar"
 import { DashboardSidebar } from "@/module/dashboard/ui/components/dashboard-sidebar"
 
 interface Props {
@@ -9,7 +10,8 @@ const Layout = ({children}: Props) =>{
     return (
         <SidebarProvider>
             <DashboardSidebar/>
-            <main className="flex flex-col h-screen w-screen bg-muted">      
+            <main className="flex flex-col h-screen w-screen bg-muted">   
+                <DashboardNavbar/>   
                  {children}
             </main>
         </SidebarProvider>
