0d438bfff04bbdd0a3a106bf26d3b654f3589ec8
Narendra2127 <narendra.nw21@gmail.com>
2025-07-04 19:11:17 +0530

    trpc setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { TRPCReactProvider } from "@/trpc/client";
 
 const inter = Inter({
 subsets: ["latin"],
@@ -19,12 +20,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <TRPCReactProvider>   <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className}  antialiased`}
       >
         {children}
       </body>
     </html>
+    </TRPCReactProvider>
+ 
   );
 }
